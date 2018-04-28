4f93fd7eea6470c1e7b2297e785e0ef7b42de69e
j-e-n-n-y-t-h-e-k-o-r-e-a-n <38788141+j-e-n-n-y-t-h-e-k-o-r-e-a-n@users.noreply.github.com>
2018-04-28 15:22:49 +0800

    Update Deductions.sql

--- Deductions.sql
+++ Deductions.sql
@@ -3,8 +3,10 @@
 
 Create Table deductions(
 codeDescription     Int Not Null Auto_Increment Unique Primary Key,
+facultyno           Int,
 effectivityDate   	date Not Null,
-terminationDate		date Not Null,
+terminationDate	  	date Not Null,
 deductedAmount      Float(2) Default 0,
-total_deductions	Float(2) Default 0 Not Null
+total_deductions	  Float(2) Default 0 Not Null,
+FOREIGN KEY(facultyno) REFERENCES faculty(facultyno)
 );
