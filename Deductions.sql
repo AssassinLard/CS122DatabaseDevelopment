59e9ca940487112938fe0fbf891ec26c06d7b7ad
j-e-n-n-y-t-h-e-k-o-r-e-a-n <38788141+j-e-n-n-y-t-h-e-k-o-r-e-a-n@users.noreply.github.com>
2018-04-28 15:26:56 +0800

    Update Deductions.sql

--- Deductions.sql
+++ Deductions.sql
@@ -8,5 +8,5 @@ effectivityDate   	date Not Null,
 terminationDate	  	date Not Null,
 deductedAmount      Float(2) Default 0,
 total_deductions	  Float(2) Default 0 Not Null,
-FOREIGN KEY(facultyno) REFERENCES faculty(facultyno)
+FOREIGN KEY(employeeno) REFERENCES employee(employeeno)
 );
