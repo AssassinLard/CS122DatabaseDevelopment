a90423837e3854c9ed6902bbd9e75de034b53a41
riccirecto <34016760+riccirecto@users.noreply.github.com>
2018-05-13 19:36:43 +0800

    Add files via upload

--- admin_index.php
+++ admin_index.php
@@ -13,7 +13,6 @@
         <header>
 
             <div class="topnav">
-                <ul>User Number</ul>
                 <ul>
                     <div id = "SplashScreen">
                     <div id = "Time"></div>
@@ -24,17 +23,22 @@
         </header>
 
         <nav class ="side">
-            <a class="tablinks" onclick="openTab(event, 'HOME')">Home</a> 
+            <a href="admin_index.php" >Home</a> 
             <a class="tablinks" onclick="openTab(event, 'HELP')">Help</a>
             <a class="tablinks" onclick="openTab(event, 'FACULTY')">Faculty</a>
             <a class="tablinks" onclick="openTab(event, 'PAYROLL')">Payroll</a>
             <a class="tablinks" onclick="openTab(event, 'CREDITS')">Credits</a>
-            <a class="tablinks" href="index.html">Logout</a>
+            <a class="tablinks" id="logout" href="index.html" style="color:black" >Logout</a>
         </nav>
 
         <div>
             <article>
+			
                 <div id="HOME" class="tabcontent">
+				<hr>
+				<br />
+				<br />
+				<br />
                   <h1>Welcome to DigiLog!<br></h1>
                     <h2><br><br>Uses:</h2>
                     <p><br>Checking of Faculty Attendance<br>Checking of Faculty Wage<br><br></p>
@@ -44,9 +48,16 @@
                     <p><br>Barangka National High School<br><br></p>
                     <h2><br><br>Created with:</h2>
                     <p><br>HTML, CSS, PHP, and MySQL<br><br></p>
+				<br />
+				<br />
+				<br />
                 </div>
 
                 <div id="HELP" class="tabcontent">
+				<hr>
+				<br />
+				<br />
+				<br />
                   <h1>Help and FAQs<br></h1>
                     <h2><br><br>Can I use this outside of BNHS?</h2>
                     <p><br>No, this is only accessible within the school.<br><br></p>
@@ -56,16 +67,22 @@
                     <p><br>As of the moment no, but should there be the need to do so, it will be possible in the future after some updates.<br><br></p>
                     <h2><br><br>Are the calculations done in real time?</h2>
                     <p><br>Yes, the calculations for wages are done in real time and are based on the login time of teachers.<br><br></p>
-                </div>
+                <br />
+				<br />
+				<br />
+				</div>
 
                 <div id="FACULTY" class="tabcontent">
+				<hr>
+				<br />
+				<br />
+				<br />
                     <h1>BNHS Faculty<br></h1>
                     <br><br>
                     <?php
                         $user = 'root';
                         $pass = '';
                         $db = 'dtrdb';
-
                         $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
                         $sql = "SELECT employeeno, CONCAT(fname,' ',lname) 'name' FROM employee;";
                         $result = mysqli_query($db, $sql);
@@ -93,16 +110,22 @@
                     <?php
                         mysqli_close($db);
                     ?>
+				<br />
+				<br />
+				<br />
                 </div>
 
                 <div id="PAYROLL" class="tabcontent">
+				<hr>
+				<br />
+				<br />
+				<br />
                     <h1>Payroll</h1>
-                    <br><br>
+                    <br>
                     <?php
                         $user = 'root';
                         $pass = '';
                         $db = 'dtrdb';
-
                         $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
                         $sql = "SELECT employeeno, CONCAT(fname,' ',lname) 'name', exempted_amount 'salary' FROM employee;";
                         $result = mysqli_query($db, $sql);
@@ -132,16 +155,26 @@
                     <?php
                         mysqli_close($db);
                     ?>
+				<br />
+				<br />
+				<br />
                 </div>
 
                 <div id="CREDITS" class="tabcontent">
+				<hr>
+				<br />
+				<br />
+				<br />
                     <h1>Developer Credits<br></h1>
-                    <h2><br><br>Front-end developers:</h2>
-                    <p><br>Alfonso Dolina<br>Ricci Recto<br><br></p>
-                    <h2><br><br>Back-end developers:</h2>
+                    <h2><br>Front-end developers:</h2>
+                    <p><br>Alfonso Dolina<br>Ricci Recto<br></p>
+                    <h2><br>Back-end developers:</h2>
                     <p><br>Mico Aquino<br>Jennifer Kim<br>Roderick Ko<br>Christian Qui</p>
-                    <h2><br><br>Brought to you by:</h2>
+                    <h2><br><br><br>Brought to you by:</h2>
                     <p><br>The CS122 Students of <br>Ateneo de Manila University <br>SY 2017-2018<br><br></p>
+				<br />
+				<br />
+				<br />
                 </div>
             </article>
         </div>
@@ -174,7 +207,6 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -188,5 +220,4 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
 </script>
